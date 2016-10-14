4653dabdb10a02aea76a8ca663bc496e30ad61d8
fbsender <fbsender@Bantang2deMacBook-Air.local>
2016-10-14 15:35:15 +0800

    article

--- app/controllers/article_controller.rb
+++ app/controllers/article_controller.rb
@@ -5,4 +5,11 @@ class ArticleController < ApplicationController
   end
   def new
   end
+  def show
+    @article = Article.where(id: params[:id].to_i).select(:id,:article_content,:img_url,:title,:keywords,:description).take
+    not_found if @article.nil?
+    @path = "#{request.path}/"
+    @contents = JSON.parse(@article.article_content)
+    @articles = Article.select(:id,:title).sample(5)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "guize#yumaoqiu"
-  get "/article/edit/", to: "article#edit"
+  get "/article/:id", to: "article#show"
+  get "/article/:id/update", to: "article#update"
 end
