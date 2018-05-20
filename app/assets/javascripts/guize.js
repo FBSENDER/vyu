c6e6d5e1bcea89199c2eecefba22f10a1b2f70c4
fbsender <lmctrello@gmail.com>
2018-05-20 19:30:11 +0800

    update xcx.jpg

--- app/assets/javascripts/guize.js
+++ app/assets/javascripts/guize.js
@@ -11,18 +11,25 @@ function guize(){
   });
   $('body').scrollspy({target: '#doc-sidebar'});
   $(window).scroll(function(){
-    var $ul = $('#doc-sidebar ul')
+    var $ul = $('#doc-sidebar ul');
+    var $rd = $('#related_right');
     if($(this).scrollTop() > 80){
       if($ul.css('top') != '20px'){
         $ul.animate({top: "20px"},0);
         $('#bt_logo').removeClass('hide');
         $('#bt_logo').fadeIn("slow");
       }
+      if($rd.css('top') != '-50px'){
+        $rd.animate({top: "-50px"},0);
+      }
     }
     else{
       if($ul.css('top') == '20px'){
         $ul.animate({top: "72px"},0);
         $('#bt_logo').fadeOut("slow");
+      }
+      if($rd.css('top') == '-50px'){
+        $rd.animate({top: "0"},0);
       }
     };
     if($(this).scrollTop() != 0) {
