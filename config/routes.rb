55737198911f263811c72029f57e059cfb753abf
fbsender <lmctrello@gmail.com>
2018-08-08 15:34:58 +0800

    heigouqi

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "shuiguo#jidanguo", constraints: {host: "www.jidanguo.top"}
+  root "shuiguo#heigouqi", constraints: {host: "www.iheigouqi.top"}
   root "guize#yumaoqiu"
   get "/article/", to: "article#index"
   get "/article/:id", to: "article#show"
