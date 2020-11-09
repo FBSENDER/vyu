7412bfb429ec6715dab9d3393c509a18a004a247
fbsender <lmctrello@gmail.com>
2020-11-09 18:52:44 +0800

    vxixi update

--- app/controllers/guize_controller.rb
+++ app/controllers/guize_controller.rb
@@ -2,7 +2,7 @@ require 'link'
 
 class GuizeController < ApplicationController
   def yumaoqiu
-    #redirect_pc_to_mobile
+    redirect_pc_to_mobile
     @robot = is_robot?
     #@articles = Article.where(status: 1).select(:id,:title).sample(10)
     @links = Link.where(status: 1).to_a
